e023d1d62c0e7f10415de042c8b6f6e894859aa8
TeddyMuli <tmuli974@gmail.com>
2024-11-06 21:46:37 +0300

    add filter_datum

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+""" Module to filter logs
+"""
+import re
+
+
+def filter_datum (fields, redaction, message, separator):
+    """ Function to return the log message obfuscated
+    """
+    pattern = f"({'|'.join(fields)})=[^{separator}]*"
+    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
